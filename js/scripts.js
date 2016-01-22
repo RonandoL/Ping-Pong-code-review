76a9b14c6e2e96c3a243a321a96f622e3b316581
Ronando <ronando.long@gmail.com>
2016-01-22 11:24:34 -0800

    It will not display error message for NaN input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,5 @@
 // Business Logic
 
-// Error Handling: (text and empty input)
-var errorHandling = function(number){
-  var output = [];
-
-  if (isNaN(number) || (number === "")) {
-    output.push("error message");
-  }
-  return output;
-};
-
-// Main PingPong:
-var pingPong = function(number){
-  var ping = "ping";   // variables used instead of hardcoding values into code
-  var pong = "pong";   // makes for happier coder who has to change a lot later on
-  var number1 = "3";
-  var number2 = "5";
-  var output = [];
-
-  for (var i = 1; i <= number; i++) {
-    if ((i % number1 === 0) && (i % number2 === 0)) {
-      output.push("<li>" + ping + "-" + pong + "</li>");
-    } else if (i % number1 === 0) {
-      output.push("<li>" + ping + "</li>");
-      console.log(output);
-    } else if (i % number2 === 0) {
-      output.push("<li>" + pong + "</li>");
-    } else {
-      output.push("<li>" + i + "</li>");
-    }
-  }
-  return output.join(' ');
-};
-
 //Negative Numbers
 var negativeNumbers = function(number){
   var ping = "ping";
@@ -57,9 +24,36 @@ var negativeNumbers = function(number){
   }
 };
 
-// --------------------------------------------------------
+// -------------
 
-// User Interface Logic
+// Main PingPong:
+var pingPong = function(number){
+  var ping = "ping";   // variables used instead of hardcoding values into code
+  var pong = "pong";   // makes for happier coder who has to change a lot later on
+  var number1 = "3";
+  var number2 = "5";
+  var output = [];    // numbers pushed to array that is spit out to browser
+
+  for (var i = 1; i <= number; i++) {
+    if (isNaN(number) || (number === "")) {
+      output.push("error message");     // error message not working;
+    } else if ((i % number1 === 0) && (i % number2 === 0)) {
+      output.push("<li>" + ping + pong + "</li>");
+    } else if (i % number1 === 0) {
+      output.push("<li>" + ping + "</li>");
+    } else if (i % number2 === 0) {
+      output.push("<li>" + pong + "</li>");
+    } else {
+      output.push("<li>" + i + "</li>");
+    }
+  }
+  return output.join(' ');
+};
+
+
+// User Interface Logic ===========================================================
+
+
 $(document).ready(function() {
   $("form.pingPong").submit(function(event) {
     var result = pingPong(parseInt($("input.userInput").val()));
